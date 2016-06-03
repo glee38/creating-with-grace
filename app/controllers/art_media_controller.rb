82170490ac357c15af76ec437c1175eb7b548001
glee38 <glee3388@gmail.com>
2016-06-02 21:19:21 -0400

    add controller actions and implement views

--- app/controllers/art_media_controller.rb
+++ app/controllers/art_media_controller.rb
@@ -1,2 +1,18 @@
 class ArtMediaController < ApplicationController
+  before_action :set_art_medium, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @art_media = ArtMedium.all
+  end
+
+  def show
+
+  end
+
+  private
+
+  def set_art_medium
+    @art_medium = ArtMedium.find(params[:id])
+  end
+
 end
