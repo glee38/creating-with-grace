34f1356890595ccf6c915f7dcc9cf983547c79ad
glee38 <glee3388@gmail.com>
2016-06-02 21:20:17 -0400

    add controller actions and implement show/index views

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,16 @@
 class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show, :update, :edit, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
 end
