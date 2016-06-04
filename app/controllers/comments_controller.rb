d9ad91693ccb6efe137fe9ce0c24537f0aa463a3
glee38 <glee3388@gmail.com>
2016-06-04 17:08:41 -0400

    add new, create, destory actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,20 @@
 class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
 end
