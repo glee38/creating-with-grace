5a17a682354a10d48e5d2a37563325a79cfe77bc
glee38 <glee3388@gmail.com>
2016-06-03 23:22:27 -0400

    implement redcarpet helper method and add to post show view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,15 @@ module ApplicationHelper
     end
   end
 
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(auto_link: true, no_intraemphas: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer)
+    return markdown.render(text).html_safe
+  end
+
+  def markdown_comment(text)
+    renderer = Redcarpet::Render::HTML.new(filter_html: true, auto_link: true, no_intraemphas: true, hard_wrap: true)
+    markdown = Redcarpet::Markdown.new(renderer)
+    return markdown.render(text).html_safe
+  end
 end
