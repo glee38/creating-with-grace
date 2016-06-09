beb656637a56566ca0b3cd2e9a871a6b91c81249
glee38 <glee3388@gmail.com>
2016-06-09 00:03:16 -0400

    modify actions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,29 +1,43 @@
 class CommentsController < ApplicationController
+  before_action :set_post, only: [:create, :edit, :update, :destroy]
   def new
   end
 
   def create
-    @post = Post.find(params[:post_id])
     @comment = @post.comments.build(comment_params)
+    @comment.user = current_user
     
     if @comment.save
       flash[:notice] = "Comment was successfully added."
       redirect_to post_path(@post)
     else
-      flash[:alert] = "Comment could not be added: content cannot be blank."
+      flash[:alert] = "Comment could not be added: Content cannot be blank."
       render template: 'posts/show' 
     end
   end
 
-  def destroy
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy 
     @comment = Comment.find(params[:id])
     @comment.destroy
+
+    flash[:notice] = "Comment was successfully deleted."
+    redirect_to post_path(@post)
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:content)
+  end
+
+  def set_post
+    @post = Post.find(params[:post_id])
   end
 
 end
