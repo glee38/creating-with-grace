33f2fd9b6068a63ad0eec1f33e5b1ffd76edb77d
glee38 <glee3388@gmail.com>
2016-06-21 15:45:41 -0400

    add profile params

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,33 @@
 class ProfilesController < ApplicationController
+  before_action :set_profile, only: [:show, :edit, :update, :destroy]
+
+  def index  
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:avatar. :bio)
+
+  def set_profile
+    @profile = current_user.profile
+  end
 end
