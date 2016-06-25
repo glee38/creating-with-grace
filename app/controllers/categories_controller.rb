8c9fa30a0527da04a8eda2e75b05571dd6f30cb0
glee38 <glee3388@gmail.com>
2016-06-25 19:35:15 -0400

    add find_by_slug instead of find

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,6 +11,6 @@ class CategoriesController < ApplicationController
   private
 
   def set_category
-    @category = Category.find(params[:id])
+    @category = Category.find_by_slug(params[:id])
   end
 end
