875719197fdd91050de6a33134f948f6dd4a7343
glee38 <glee3388@gmail.com>
2016-07-01 16:04:42 -0400

    add verify_completed_sign_up method and before_action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,21 @@ class ApplicationController < ActionController::Base
   layout 'application'
   helper_method :to_slug
 
+  before_action :verify_completed_sign_up
+
   def to_slug
     downcase.strip.gsub(" ", "-")
+  end
+
+  private
+
+  def verify_completed_sign_up
+    if user_signed_in?
+      if current_user.username.nil? || current_user.name.nil?
+        flash.alert = "Almost done! Just one more step to complete sign up." 
+        redirect_to registration_steps_personal_path
+      end
+    end
   end
 
 end
