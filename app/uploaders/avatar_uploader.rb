580cefee50fc2ac620af54f1236858027648e2fd
glee38 <glee3388@gmail.com>
2016-07-04 23:23:30 -0400

    include rmagick, add methods

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -3,7 +3,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
+  include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -16,6 +16,23 @@ class AvatarUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def unsharp_mask(radius, sigma, amount, threshold)
+      # refer to this http://www.imagemagick.org/Usage/resize/#resize_unsharp
+      manipulate! do |img|
+        img.unsharp_mask radius, sigma, amount, threshold
+        img = yield(img) if block_given?
+        img
+      end
+    end
+
+    def quality(percentage)
+      manipulate! do |img|
+        img.write(current_path){ self.quality = percentage } unless img.quality == percentage
+        img = yield(img) if block_given?
+        img
+      end
+    end
+
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
@@ -23,6 +40,12 @@ class AvatarUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+
+  version :thumb do
+    process resize_to_fill: [180,180]
+    process quality: 100
+    process :unsharp_mask => [0.0, 0.5, 0.5, 0.008]
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
