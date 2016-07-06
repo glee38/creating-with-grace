ea32ccf0618cbb40e45a0927224c991d837df9b7
glee38 <glee3388@gmail.com>
2016-07-05 23:39:15 -0400

    add user_not_authorized method to rescue from Pundit exceptions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  include Pundit
+
   protect_from_forgery with: :exception
   layout 'application'
   helper_method :to_slug
 
   before_action :verify_completed_sign_up
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   def to_slug
     downcase.strip.gsub(" ", "-")
@@ -19,6 +23,16 @@ class ApplicationController < ActionController::Base
         flash.alert = "Almost done! Just one more step to complete sign up." 
         redirect_to registration_steps_personal_path
       end
+    end
+  end
+
+  def user_not_authorized(exception)
+    if user_signed_in?
+      flash[:error] = "You must be an admin to do that."
+      redirect_to(request.referrer || root_path)
+    else
+      flash[:error] = "Please login or sign up."
+      redirect_to(request.referrer || root_path)
     end
   end
 
